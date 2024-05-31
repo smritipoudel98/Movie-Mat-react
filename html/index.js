3516d2c321ec16a2b6f48ff117c88bbb53f08eb2
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-05-31 16:52:30 +0545

    funct

--- html/index.js
+++ html/index.js
@@ -8,3 +8,24 @@ const formSubmission = () => {
     console.log(value);
   }
 };
+
+//write a function to check if the number is even or odd
+
+const checkNumber = (a) =>
+  a % 2 === 0 ? console.log("Number is even") : console.log("Number is odd");
+
+const b = checkNumber(3);
+console.log(b);
+
+//write a function that convert fullname to propercase
+
+const fullname = (a) =>
+  a
+    .split(" ")
+    .map(
+      (word) =>
+        word.slice(0, 1).toUpperCase() +
+        word.slice(1, word.length).toLowerCase()
+    )
+    .join(" ");
+console.log(fullname("smriTi poudel"));
