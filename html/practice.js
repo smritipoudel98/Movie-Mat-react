fd0be1c707ca4a69fa8c339d77e479e51d64e285
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-06-03 11:07:17 +0545

    qs

---
+++ html/practice.js
@@ -0,0 +1,27 @@
+/*1.
+let arr = [1,1,1,2,2,3,3,4];
+count occurance = 3
+result= 2
+Write a fn that check the occurance of number
+ 
+2.
+let arr = [1,2,3,4,4,5,5];
+// Write a js function that remove the duplicates from arr
+result [1,2,3,4,5]
+ 
+3.
+let arr1= ['icecream', 'pizza', 'momo']
+let arr2= ['fruits', 'pizza', 'veggies'];
+// find the common item in both arrays
+// result ['pizza']
+ 
+ 
+4. Write a js function to sort the follwing array of object by title value;
+const books =  [
+{author: "Raktim", title: "The Road ahead"},
+{author: "Alish", title: "Harry Potter"},
+{author: "Prem", title: "Carry On"},
+{author: "Smriti", title: "Big Ben"},
+]
+// result [big ben, carry on,  harry potter, the road]
+*/
